2cbe31cd643132f9d9cdeee41addc3b832cb9160
Tae Hee Lee <liver1379@naver.com>
2024-08-10 00:54:39 +0900

    TagRepository에 jpaQuery 추가

--- src/main/java/ezen/blog/tag/TagRepository.java
+++ src/main/java/ezen/blog/tag/TagRepository.java
@@ -3,5 +3,5 @@ package ezen.blog.tag;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TagRepository extends JpaRepository<Tag, Long> {
-    Tag findByName(String name);
+    Tag findByIdAndIsDeletedFalse(Long id);
 }
